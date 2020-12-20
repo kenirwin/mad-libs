584b96c5bc02f68ccfe115954ff146e3cda07f5c
Ken Irwin <kirwin@wittenberg.edu>
2020-12-20 16:30:35 -0500

    added replace.sh

---
+++ replace.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# sed 's/__([A-Z ]+)__/<span class="input" data-placeholder="\1"><\/span>/g' $1
+
+perl -pi.bak -e 's/__([A-Z,\(\) ]+)__/<span class="input" data-placeholder="\1"><\/span>/g' $1
+
+# echo $1
